fc3cde955a565f46d683a825191811c8da97d201
an4 <ad12461@my.bristol.ac.uk>
2015-02-03 18:50:25 +0000

    Basic stage 3 and 4

--- modmul.c
+++ modmul.c
@@ -26,21 +26,21 @@ void stage1() {
 
 	while(!feof(stdin)) {
 
-	initialize_and_read(e);
-	initialize_and_read(m);
+		initialize_and_read(e);
+		initialize_and_read(m);
 
-  	mpz_init( c );
-  	mpz_powm(c, m, e, N);
+		mpz_init( c );
 
-	
-  	gmp_printf( "%Zx\n", c );
+		mpz_powm(c, m, e, N);
 
-  	mpz_clear( N );
-  	mpz_clear( e );
-  	mpz_clear( m );
-  	mpz_clear( c );
+		gmp_printf( "%Zx\n", c );
 
-  	initialize_and_read(N);
+		mpz_clear( N );
+		mpz_clear( e );
+		mpz_clear( m );
+		mpz_clear( c );
+
+		initialize_and_read(N);
 
   	}
 }
@@ -55,7 +55,40 @@ Perform stage 2:
 
 void stage2() {
 
-  // fill in this function with solution
+	// fill in this function with solution
+	mpz_t N, d, p, q, d_p, d_q, i_p, i_q, c, m;
+
+	initialize_and_read(N);
+
+	while(!feof(stdin)) {
+
+		initialize_and_read(d);
+		initialize_and_read(p);
+		initialize_and_read(q);
+		initialize_and_read(d_p);
+		initialize_and_read(d_q);
+		initialize_and_read(i_p);
+		initialize_and_read(i_q);
+		initialize_and_read(c);
+
+		mpz_init(m);
+
+		mpz_powm(m, c, d, N);
+
+		gmp_printf( "%Zx\n", m );
+
+		mpz_clear(N);
+		mpz_clear(d);
+		mpz_clear(p);
+		mpz_clear(q);
+		mpz_clear(d_p);
+		mpz_clear(d_q);
+		mpz_clear(i_p);
+		mpz_clear(i_q);
+		mpz_clear(c);
+
+		initialize_and_read(N);
+	}
 
 }
 
@@ -69,7 +102,43 @@ Perform stage 3:
 
 void stage3() {
 
-  // fill in this function with solution
+	// fill in this function with solution
+	mpz_t p, q, g, h, m, c1, c2, k;
+
+	initialize_and_read(p);
+
+	while(!feof(stdin)) {
+
+		initialize_and_read(q);
+		initialize_and_read(g);
+		initialize_and_read(h);
+		initialize_and_read(m);
+
+		mpz_init(c1);
+		mpz_init(c2);
+		mpz_init(k);
+
+		int i_k = 1;
+		mpz_set_ui(k, i_k);
+
+		mpz_mod(k, k, q);
+		mpz_powm(c1, g, k, p);
+		mpz_pow_ui(h, h, i_k);
+		mpz_mul(c2, m, h);
+		mpz_mod(c2, c2, p);
+
+		gmp_printf( "%Zx\n", c1 );
+		gmp_printf( "%Zx\n", c2 );
+
+		mpz_clear(p);
+		mpz_clear(q);
+		mpz_clear(g);
+		mpz_clear(h);
+		mpz_clear(m);
+
+		initialize_and_read(p);
+
+	}
 
 }
 
@@ -83,7 +152,42 @@ Perform stage 4:
 
 void stage4() {
 
-  // fill in this function with solution
+ 	// fill in this function with solution
+	mpz_t p, q, g, x, c1, c2, m;
+
+	initialize_and_read(p);
+
+	while(!feof(stdin)) {
+		
+		initialize_and_read(q);
+		initialize_and_read(g);
+		initialize_and_read(x);
+		initialize_and_read(c1);
+		initialize_and_read(c2);
+
+		mpz_init(m);
+
+		mpz_mod(x, x, q);
+		mpz_neg(x, x);
+		mpz_add(x, x, q);
+
+		mpz_powm(c1, c1, x, p);
+		mpz_mul(m, c1, c2);
+		mpz_mod(m, m, p);
+
+		gmp_printf( "%Zx\n", m );
+
+		mpz_clear(p);
+		mpz_clear(q);
+		mpz_clear(g);
+		mpz_clear(x);
+		mpz_clear(c1);
+		mpz_clear(c2);
+		mpz_clear(m);
+
+		initialize_and_read(p);
+
+	}
 
 }
 
